d717c7c28a53e11db3c7c777492bc2b0bb4173f8
Dhaiwat Kabaria <dhaiwat.kabaria@kellogg.northwestern.edu>
2026-03-08 15:39:59 -0500

    fix: updateListing merges patch into listing instead of replacing

--- frontend/src/store/useStore.js
+++ frontend/src/store/useStore.js
@@ -22,8 +22,13 @@ const useStore = create((set) => ({
   setModelConfig: (config) => set((s) => ({ modelConfig: { ...s.modelConfig, ...config } })),
   setJobResult: (jobResult) => set({ jobResult }),
   setQaResult: (qaResult) => set({ qaResult }),
-  updateListing: (listing) =>
-    set((s) => ({ jobResult: { ...s.jobResult, listing } })),
+  updateListing: (patch) =>
+    set((s) => ({
+      jobResult: {
+        ...(s.jobResult ?? {}),
+        listing: { ...(s.jobResult?.listing ?? {}), ...patch },
+      },
+    })),
   reset: () => set({ images: [], notes: '', jobResult: null, qaResult: null }),
 }))
 
